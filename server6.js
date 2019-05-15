c04e2b858045c9abb3d46a11a9f305673de07274
yamamotorinpei <1001rimpei@gmail.com>
2019-05-15 07:36:25 +0000

    serverを追加

--- server6.js
+++ server6.js
@@ -4,7 +4,9 @@ const server = express();
 server.get('/', function( req, res ) {
     res.send( 'Hello, world' );
 });
-
+server.get('/morning', function( req , res ) {
+  res.send( 'Good morning, World' );
+});
 server.listen( 80, function() {
     console.log( 'listening on port 80' );
 });
--- server8.js
+++ server8.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 server.get('/', function( req, res ) {
     let sorting = req.query.sorting || '人口';
     let number = req.query.number || 10;
-    let query = 'select id, 都道府県, ' + sorting + ' from example order by ' + sorting + ' desc limit ' + number + ';';
+    let query = 'select id, 都道府県,'+ sorting + ' as population from example order by ' + sorting + ' desc limit ' + number + ';';
     console.log( query );
     connection.query( query, (error, rows, fields) => {
         if( error ) {
@@ -23,7 +23,11 @@ server.get('/', function( req, res ) {
         res.render( 'sql2.ejs', { content: rows });
     });
 });
-
+server.get('/web', function( req, res) {
+    let sorting = req.query.sorting || '学生数';
+    let number = req.query.number || 5;
+    let query = 'select id, 都道府県'
+});
 server.listen( 80, function() {
     console.log( 'listening on port 80' );
 });
