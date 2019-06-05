87fc5ebc012fdb0a43b4aeb1f2c89c4a79295f0b
yamamotorinpei <49706282+yamamotorinpei@users.noreply.github.com>
2019-06-05 15:35:49 +0900

    Update and rename server8.js to server9.js
    let query = select batting.id,batting.year,batting.HR,player.name
        form batting
        inner join player on batting.player_id=player.id
        limit 10;

--- server9.js
+++ server9.js
@@ -11,10 +11,11 @@ var connection = mysql.createConnection({
     database: 'web'
 });
 
-server.get('/', function( req, res ) {
-    let sorting = req.query.sorting || '人口';
-    let number = req.query.number || 10;
-    let query = 'select id, 都道府県,'+ sorting + ' as population from example order by ' + sorting + ' desc limit ' + number + ';';
+server.get('/player/', function( req, res ) {
+    let query = select batting.id,batting.year,batting.HR,player.name
+    form batting
+    inner join player on batting.player_id=player.id
+    limit 10;
     console.log( query );
     connection.query( query, (error, rows, fields) => {
         if( error ) {
@@ -23,11 +24,7 @@ server.get('/', function( req, res ) {
         res.render( 'sql2.ejs', { content: rows });
     });
 });
-server.get('/web', function( req, res) {
-    let sorting = req.query.sorting || '学生数';
-    let number = req.query.number || 5;
-    let query = 'select id, 都道府県'
-});
+
 server.listen( 80, function() {
     console.log( 'listening on port 80' );
 });
